ae044be84ede0c9f1d5f02e4001c9a006a7f527a
NataliaZanegina <nataliazanegina@yandex.ru>
2024-07-05 15:00:58 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+
+    public static void main(String[] args){
+        RedBlackTree redBlackTree = new RedBlackTree();
+        for (int i = 1; i <= 100; i++) {
+            redBlackTree.add(i);
+        }
+    }
+
+}
